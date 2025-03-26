565c1736b256ac3027133af3a07c3f97c09b29df
pr882260 <pr882260@gmail.com>
2025-03-26 19:45:55 +0900

    1

---
+++ src/main/java/haedalSpring/haedal/HaedalApplication.java
@@ -0,0 +1,13 @@
+package haedalSpring.haedal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaedalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaedalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haedalSpring/haedal/HaedalApplicationTests.java
@@ -0,0 +1,13 @@
+package haedalSpring.haedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
